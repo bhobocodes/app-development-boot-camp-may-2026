28661d5733b94bdbb7e4a7efafff61e4f842c891
Durjoy <bhobotosh345@gmail.com>
2026-05-06 10:11:34 +0600

    class extends

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -2,3 +2,16 @@ import 'package:flutter/material.dart';
 import 'register_screen.dart';
 import 'dashboard_screen.dart';
 
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  bool obscure = true;
+
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passController = TextEditingController();
+
